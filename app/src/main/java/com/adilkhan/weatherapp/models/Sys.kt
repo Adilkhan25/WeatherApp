d9ea3fef4b138f7e1957b9dafad1f80a9a39a5a4
Adil Khan <Adil03320@gmail.com>
2023-05-24 21:52:42 +0530

    prepared model according to json objects

---
+++ app/src/main/java/com/adilkhan/weatherapp/models/Sys.kt
@@ -0,0 +1,11 @@
+package com.adilkhan.weatherapp.models
+
+import java.io.Serializable
+
+data class Sys(
+    val type: Int,
+    val message: Double,
+    val country: String,
+    val sunrise: Int,
+    val sunset: Int
+) : Serializable
